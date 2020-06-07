d34a3cdcb1c9e8b43776f6d9727c166f62639a5f
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-07 11:50:52 +0200

    Sección 16 Clase 15 - Obtener productos de la tienda por plataforma seleccionada

--- src/resolvers/query/shop-product.ts
+++ src/resolvers/query/shop-product.ts
@@ -2,11 +2,24 @@ import { IResolvers } from 'graphql-tools';
 import ShopProductsService from '../../services/shop-product.service';
 const resolversShopProductsQuery: IResolvers = {
   Query: {
-    shopProducts(_, {page, itemsPage, active}, context) {
-      return new ShopProductsService(_, {
-        pagination: { page, itemsPage}
-    }, context).items(active);
-    }
+    shopProducts(_, { page, itemsPage, active }, context) {
+      return new ShopProductsService(
+        _,
+        {
+          pagination: { page, itemsPage },
+        },
+        context
+      ).items(active);
+    },
+    shopProductsPlatforms(_, { page, itemsPage, active, platform }, context) {
+      return new ShopProductsService(
+        _,
+        {
+          pagination: { page, itemsPage },
+        },
+        context
+      ).items(active, platform);
+    },
   },
 };
 
--- src/services/shop-product.service.ts
+++ src/services/shop-product.service.ts
@@ -1,25 +1,41 @@
 import { COLLECTIONS, ACTIVE_VALUES_FILTER } from './../config/constants';
 import ResolversOperationsService from './resolvers-operations.service';
 
-
 class ShopProductsService extends ResolversOperationsService {
-    collection = COLLECTIONS.SHOP_PRODUCT;
-    constructor(root: object, variables: object, context: object) {
-        super(root, variables, context);
-    }
+  collection = COLLECTIONS.SHOP_PRODUCT;
+  constructor(root: object, variables: object, context: object) {
+    super(root, variables, context);
+  }
 
-    async items(active: string = ACTIVE_VALUES_FILTER.ACTIVE) {
-        let filter: object = { active: {$ne: false}};
-        if (active === ACTIVE_VALUES_FILTER.ALL) {
-          filter = {};
-        } else if (active === ACTIVE_VALUES_FILTER.INACTIVE) {
-          filter = { active: false };
-        }
-        const page = this.getVariables().pagination?.page;
-        const itemsPage = this.getVariables().pagination?.itemsPage;
-        const result = await this.list(this.collection, 'productos de la tienda', page, itemsPage, filter);
-        return { info: result.info, status: result.status, message: result.message, shopProducts: result.items };
+  async items(
+    active: string = ACTIVE_VALUES_FILTER.ACTIVE,
+    platform: string = ''
+  ) {
+    let filter: object = { active: { $ne: false } };
+    if (active === ACTIVE_VALUES_FILTER.ALL) {
+      filter = {};
+    } else if (active === ACTIVE_VALUES_FILTER.INACTIVE) {
+      filter = { active: false };
     }
+    if (platform !== '' && platform !== undefined) {
+      filter = {...filter, ...{platform_id: platform}};
+    }
+    const page = this.getVariables().pagination?.page;
+    const itemsPage = this.getVariables().pagination?.itemsPage;
+    const result = await this.list(
+      this.collection,
+      'productos de la tienda',
+      page,
+      itemsPage,
+      filter
+    );
+    return {
+      info: result.info,
+      status: result.status,
+      message: result.message,
+      shopProducts: result.items,
+    };
+  }
 }
 
 export default ShopProductsService;
