115b4a33c7d1b74266524e667e707efbd1a3e8fd
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-25 14:12:05 +0200

    Sección 22 - Clase 9 - Listar clientes básicos - Dar solución al resolver

---
+++ src/interfaces/stripe/customer.interface.ts
@@ -0,0 +1,8 @@
+export interface IStripeCustomer {
+    id: string;
+    name: string;
+    email: string;
+    description: string;
+    default_source?: string;
+    currency?: string;
+}
--- src/resolvers/query/stripe/customer.ts
+++ src/resolvers/query/stripe/customer.ts
@@ -1,8 +1,27 @@
 import { IResolvers } from 'graphql-tools';
+import StripeApi from '../../../lib/stripe-api';
+import { IStripeCustomer } from '../../../interfaces/stripe/customer.interface';
 const resolversStripeCustomerQuery: IResolvers = {
   Query: {
     async customers(_, { limit } ) {
         console.log(limit);
+        const stripe = new StripeApi().stripe;
+        return await stripe.customers.list(
+            { limit }
+        ).then((result: {has_more: boolean, data: Array<IStripeCustomer>}) => {
+            return {
+                status: true,
+                message: 'Lista cargada correctamente con los clientes seleccionados',
+                hasMore: result.has_more,
+                customers: result.data
+            };
+        }).catch( (error: Error) => {
+            return {
+                status: false,
+                message: 'Error: '.concat(error.message),
+                hasMore: false,
+            };
+        });
     },
   },
 };
