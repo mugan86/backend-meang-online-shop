b8f9bb29204c734100d51fc348162853de979377
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-25 14:51:58 +0200

    Sección 22 - Clase 12 - Validación para evitar duplicados de clientes

--- src/resolvers/mutation/stripe/customer.ts
+++ src/resolvers/mutation/stripe/customer.ts
@@ -1,10 +1,27 @@
 import { STRIPE_ACTIONS } from './../../../lib/stripe-api';
 import { IResolvers } from 'graphql-tools';
 import StripeApi, { STRIPE_OBJECTS } from '../../../lib/stripe-api';
+import { IStripeCustomer } from '../../../interfaces/stripe/customer.interface';
 
 const resolversStripeCustomerMutation: IResolvers = {
   Mutation: {
     async createCustomer(_, { name, email }) {
+      // COmprobar que el cliente no existe y en el caso de que exista,
+      // devolver diciendo que no se puede añadir
+      const userCheckExist: {
+        data: Array<IStripeCustomer>;
+      } = await new StripeApi().execute(
+        STRIPE_OBJECTS.CUSTOMERS,
+        STRIPE_ACTIONS.LIST,
+        { email }
+      );
+      if (userCheckExist.data.length > 0) {
+        // Usuario existe
+        return {
+          status: false,
+          message: `El usuario con el email ${email} ya existe en el sistema`,
+        };
+      }
       return await new StripeApi()
         .execute(STRIPE_OBJECTS.CUSTOMERS, STRIPE_ACTIONS.CREATE, {
           name,
