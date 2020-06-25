5fb8448afc3359a69640423f31ff91a9c58c2636
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-25 11:00:38 +0200

    Sección 22- Clase 7 - Clientes - Dar solución a la definición

--- src/resolvers/mutation/index.ts
+++ src/resolvers/mutation/index.ts
@@ -3,12 +3,14 @@ import resolversUserMutation from './user';
 import resolversGenreMutation from './genre';
 import resolversTagMutation from './tag';
 import resolversMailMutation from './email';
+import mutationStripeResolvers from './stripe';
 
 const mutationResolvers = GMR.merge([
     resolversUserMutation,
     resolversGenreMutation,
     resolversTagMutation,
-    resolversMailMutation
+    resolversMailMutation,
+    mutationStripeResolvers
 ]);
 
 export default mutationResolvers;
--- src/resolvers/mutation/stripe/customer.ts
+++ src/resolvers/mutation/stripe/customer.ts
@@ -1,9 +1,28 @@
 import { IResolvers } from 'graphql-tools';
+import StripeApi from '../../../lib/stripe-api';
 
 const resolversStripeCustomerMutation: IResolvers = {
   Mutation: {
-    createCustomer(_, variables, context) {
-      return;
+    async createCustomer(_, {name, email}) {
+        const stripe = new StripeApi().stripe;
+        return await stripe.customers.create(
+            {
+                name,
+                email,
+                description: `${name} (${email})`
+            }
+        ).then((result: object) => {
+            return {
+                status: true,
+                message: `El cliente ${name} se ha creado correctamente`,
+                customer: result
+            };
+        }).catch((error: Error) => {
+            return {
+                status: false,
+                message: `Error: `.concat(error.message)
+            };
+        });
     }
   },
 };
