7667171116397f246f8874abed0fbf0e944aff1e
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-25 14:26:06 +0200

    Sección 22 - Clase 10.- Paginación - Aplicado a los clientes

--- src/resolvers/query/stripe/customer.ts
+++ src/resolvers/query/stripe/customer.ts
@@ -3,11 +3,20 @@ import StripeApi from '../../../lib/stripe-api';
 import { IStripeCustomer } from '../../../interfaces/stripe/customer.interface';
 const resolversStripeCustomerQuery: IResolvers = {
   Query: {
-    async customers(_, { limit } ) {
+    async customers(_, { limit, startingAfter, endingBefore } ) {
         console.log(limit);
+        let pagination;
+        if (startingAfter !== '' && endingBefore === '') {
+            pagination = { starting_after: startingAfter};
+        } else if (startingAfter === '' && endingBefore !== '') {
+            pagination = { ending_before: endingBefore};
+        } else {
+            pagination = {};
+        }
+        console.log(pagination);
         const stripe = new StripeApi().stripe;
         return await stripe.customers.list(
-            { limit }
+            { limit, ...pagination }
         ).then((result: {has_more: boolean, data: Array<IStripeCustomer>}) => {
             return {
                 status: true,
