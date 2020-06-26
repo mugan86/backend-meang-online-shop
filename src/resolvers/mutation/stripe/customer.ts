3bcd49bf55ac5878ca2cd5e637a449ae7682b57d
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-26 11:28:51 +0200

    Sección 22 Clase 19 - Refactorización del resolver Mutation en un servicio

--- src/resolvers/mutation/stripe/customer.ts
+++ src/resolvers/mutation/stripe/customer.ts
@@ -1,107 +1,16 @@
-import { STRIPE_ACTIONS } from './../../../lib/stripe-api';
 import { IResolvers } from 'graphql-tools';
-import StripeApi, { STRIPE_OBJECTS } from '../../../lib/stripe-api';
-import { IStripeCustomer } from '../../../interfaces/stripe/customer.interface';
-import { findOneElement } from '../../../lib/db-operations';
-import { COLLECTIONS } from '../../../config/constants';
-import { IUser } from '../../../interfaces/user.interface';
-import UsersService from '../../../services/users.service';
+import StripeCustomerService from '../../../services/stripe/customer.service';
 
 const resolversStripeCustomerMutation: IResolvers = {
   Mutation: {
     async createCustomer(_, { name, email }, { db }) {
-      // COmprobar que el cliente no existe y en el caso de que exista,
-      // devolver diciendo que no se puede añadir
-      const userCheckExist: {
-        data: Array<IStripeCustomer>;
-      } = await new StripeApi().execute(
-        STRIPE_OBJECTS.CUSTOMERS,
-        STRIPE_ACTIONS.LIST,
-        { email }
-      );
-      if (userCheckExist.data.length > 0) {
-        // Usuario existe
-        return {
-          status: false,
-          message: `El usuario con el email ${email} ya existe en el sistema`,
-        };
-      }
-      return await new StripeApi()
-        .execute(STRIPE_OBJECTS.CUSTOMERS, STRIPE_ACTIONS.CREATE, {
-          name,
-          email,
-          description: `${name} (${email})`,
-        })
-        .then(async (result: IStripeCustomer) => {
-            // Actualizar en nuestra base de datos con la nueva 
-            // propiedad que es el id del cliente
-            const user: IUser = await findOneElement(db, COLLECTIONS.USERS, { email });
-            if (user) {
-                user.stripeCustomer = result.id;
-                const resultUserOperation = await new UsersService(_, { user }, {db}).modify();
-                console.log(resultUserOperation);
-                // Si el resultado es falso, no se ha ejecutado. Tenemos que borrar el cliente creado  (en Stripe)
-            }
-            return {
-            status: true,
-            message: `El cliente ${name} se ha creado correctamente`,
-            customer: result,
-          };
-        })
-        .catch((error: Error) => {
-          return {
-            status: false,
-            message: `Error: `.concat(error.message),
-          };
-        });
+      return new StripeCustomerService().add(name, email, db);
     },
     async updateCustomer(_, { id, customer}) {
-      return await new StripeApi().execute(
-        STRIPE_OBJECTS.CUSTOMERS,
-        STRIPE_ACTIONS.UPDATE,
-        id,
-        customer
-      ).then((result: IStripeCustomer) => {
-        return {
-          status: true,
-          message: `Usuario ${id} actualizado correctamente`,
-          customer: result
-        };
-      }).catch((error: Error) => {
-        return {
-          status: false,
-          message: 'Error: '.concat(error.message)
-        };
-      });
+      return new StripeCustomerService().update(id, customer);
     },
     async deleteCustomer(_, {id}, { db }) {
-      return await new StripeApi().execute(
-        STRIPE_OBJECTS.CUSTOMERS,
-        STRIPE_ACTIONS.DELETE,
-        id
-      ).then(async(result: {id: string, deleted: boolean}) => {
-        if (result.deleted) {
-          const resultOperation = await db
-          .collection(COLLECTIONS.USERS)
-          .updateOne({stripeCustomer: result.id}, { $unset: { stripeCustomer: result.id } });
-          return {
-            status: result.deleted && resultOperation ? true : false,
-            message: result.deleted && resultOperation ? 
-                      `Usuario ${id} actualizado correctamente` : 
-                      `Usuario no se ha actualizado correctamente en la base de datos nuestra`,
-          };
-        }
-        return {
-          status: false,
-          message: `Usuario ${id} NO SE HA actualizado. Compruebalo`,
-        };
-        
-      }).catch((error: Error) => {
-        return {
-          status: false,
-          message: 'Error: '.concat(error.message)
-        };
-      }); 
+      return new StripeCustomerService().delete(id, db);
     }
   },
 };
--- src/services/stripe/customer.service.ts
+++ src/services/stripe/customer.service.ts
@@ -1,5 +1,10 @@
 import StripeApi, { STRIPE_OBJECTS, STRIPE_ACTIONS } from '../../lib/stripe-api';
 import { IStripeCustomer } from '../../interfaces/stripe/customer.interface';
+import { IUser } from '../../interfaces/user.interface';
+import { findOneElement } from '../../lib/db-operations';
+import UsersService from '../users.service';
+import { COLLECTIONS } from '../../config/constants';
+import { Db } from 'mongodb';
 
 class StripeCustomerService extends StripeApi {
   // Clientes lista
@@ -40,6 +45,86 @@ class StripeCustomerService extends StripeApi {
     })
     .catch((error: Error) => this.getError(error));
   }
+  async add(name: string, email: string, db: Db) {
+      // COmprobar que el cliente no existe y en el caso de que exista,
+      // devolver diciendo que no se puede añadir
+      const userCheckExist: {
+        data: Array<IStripeCustomer>;
+      } = await new StripeApi().execute(
+        STRIPE_OBJECTS.CUSTOMERS,
+        STRIPE_ACTIONS.LIST,
+        { email }
+      );
+      if (userCheckExist.data.length > 0) {
+        // Usuario existe
+        return {
+          status: false,
+          message: `El usuario con el email ${email} ya existe en el sistema`,
+        };
+      }
+      return await new StripeApi()
+        .execute(STRIPE_OBJECTS.CUSTOMERS, STRIPE_ACTIONS.CREATE, {
+          name,
+          email,
+          description: `${name} (${email})`,
+        })
+        .then(async (result: IStripeCustomer) => {
+            // Actualizar en nuestra base de datos con la nueva 
+            // propiedad que es el id del cliente
+            const user: IUser = await findOneElement(db, COLLECTIONS.USERS, { email });
+            if (user) {
+                user.stripeCustomer = result.id;
+                const resultUserOperation = await new UsersService({}, { user }, {db}).modify();
+                console.log(resultUserOperation);
+                // Si el resultado es falso, no se ha ejecutado. Tenemos que borrar el cliente creado  (en Stripe)
+            }
+            return {
+            status: true,
+            message: `El cliente ${name} se ha creado correctamente`,
+            customer: result,
+          };
+        })
+        .catch((error: Error) => this.getError(error));
+  }
+  async update(id: string, customer: IStripeCustomer) {
+    return await new StripeApi().execute(
+        STRIPE_OBJECTS.CUSTOMERS,
+        STRIPE_ACTIONS.UPDATE,
+        id,
+        customer
+      ).then((result: IStripeCustomer) => {
+        return {
+          status: true,
+          message: `Usuario ${id} actualizado correctamente`,
+          customer: result
+        };
+      }).catch((error: Error) =>  this.getError(error));
+  }
+  async delete(id: string, db: Db) {
+    return await new StripeApi().execute(
+        STRIPE_OBJECTS.CUSTOMERS,
+        STRIPE_ACTIONS.DELETE,
+        id
+      ).then(async(result: {id: string, deleted: boolean}) => {
+        if (result.deleted) {
+          const resultOperation = await db
+          .collection(COLLECTIONS.USERS)
+          .updateOne({stripeCustomer: result.id}, { $unset: { stripeCustomer: result.id } });
+          return {
+            status: result.deleted && resultOperation ? true : false,
+            message: result.deleted && resultOperation ? 
+                      `Usuario ${id} borrado correctamente` : 
+                      `Usuario no se ha borrado correctamente en la base de datos nuestra`,
+          };
+        }
+        return {
+          status: false,
+          message: `Usuario ${id} NO SE HA borrado. Compruebalo`,
+        };
+        
+      }).catch((error: Error) =>  this.getError(error)); 
+  }
+
 }
 
 export default StripeCustomerService;
