18df3c19e5d91476dd68ccee873a00355ba9a114
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-27 15:37:34 +0200

    Sección 21 - Clase 29 - Definición del schema con lo necesario para especificar la creación de los cargos de pago

---
+++ src/resolvers/mutation/stripe/charge.ts
@@ -0,0 +1,12 @@
+import { IResolvers } from 'graphql-tools';
+
+const resolversStripeChargeMutation: IResolvers = {
+  Mutation: {
+    async chargeOrder(_, { payment }) {
+        console.log(payment);
+      // return new StripeCardService().createToken(card);
+    }
+  },
+};
+
+export default resolversStripeChargeMutation;
