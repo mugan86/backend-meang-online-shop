891fa9ae2a2b16bd79dae8e3e5a7f97fc0316a65
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-28 11:01:30 +0200

    Sección 21 - Clase 31 - Primer pago realizado - OK

--- src/services/stripe/charge.service.ts
+++ src/services/stripe/charge.service.ts
@@ -1,11 +1,21 @@
 import { IPayment } from './../../interfaces/stripe/payment.interface';
-import StripeApi from '../../lib/stripe-api';
+import StripeApi, { STRIPE_OBJECTS, STRIPE_ACTIONS } from '../../lib/stripe-api';
 
 class StripeChargeService extends StripeApi {
     async order(pay: IPayment) {
         pay.amount = +((Math.round(+pay.amount * 100) / 100).toFixed(2));
         pay.amount *= 100;
         console.log(pay);
+        return await this.execute(
+            STRIPE_OBJECTS.CHARGES,
+            STRIPE_ACTIONS.CREATE,
+            pay
+        ).then((_: object) => {
+            return {
+                status: true,
+                message: 'Pago procesardo correctamente'
+            };
+        }).catch((error: Error) => this.getError(error));
     }
 }
 
