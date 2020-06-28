a62285c0a90b8e0f5988e17bfe76a3cbb9cc953c
Anartz Mugika Ledo <mugan86@gmail.com>
2020-06-28 13:37:54 +0200

    Sección 21 - Clase 34.- Comprobaciones del cliente si existe y si coge token y huella dactilar para asegurar que metemos información correcta - Parte 1

--- src/services/stripe/charge.service.ts
+++ src/services/stripe/charge.service.ts
@@ -1,8 +1,23 @@
 import { IPayment } from './../../interfaces/stripe/payment.interface';
 import StripeApi, { STRIPE_OBJECTS, STRIPE_ACTIONS } from '../../lib/stripe-api';
+import StripeCustomerService from './customer.service';
 
 class StripeChargeService extends StripeApi {
+    private async getClient(customer: string) {
+        return new StripeCustomerService().get(customer);
+    }
     async order(pay: IPayment) {
+        // Cliente buscar info detallada
+        const customerData = await this.getClient(pay.customer);
+        if (customerData.status) {
+            console.log('Cliente asignado correcamente');
+        } else {
+            console.log('No hay cliente');
+            return {
+                status: false,
+                message: 'El cliente seleccionado no existe y no podremos realizar pagos con esta información'
+            };
+        }
         pay.amount = +((Math.round(+pay.amount * 100) / 100).toFixed(2));
         pay.amount *= 100;
         console.log(pay);
