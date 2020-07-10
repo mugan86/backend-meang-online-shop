bed6b6b26a136334023e8224cdab1587128beb8d
Anartz Mugika Ledo <mugan86@gmail.com>
2020-07-10 10:30:48 +0200

    Sección 23 - Clase 3.- Resolver Susbcription updateStockProduct - Subscription AsynIterator

--- src/resolvers/subscription/shop-product.ts
+++ src/resolvers/subscription/shop-product.ts
@@ -1,9 +1,10 @@
 import { IResolvers } from 'graphql-tools';
+import { SUBSCRIPTIONS_EVENT } from '../../config/constants';
 
 const resolversShopProductSubscription: IResolvers = {
-    Query: {
-        async updateStockProduct(_, ___, { pubsub }) {
-            console.log('Actualizando stock');
+    Subscription: {
+        updateStockProduct: {
+            subscribe: (_, __, { pubsub}) => pubsub.asyncIterator(SUBSCRIPTIONS_EVENT.UPDATE_STOCK_PRODUCT),
         }
     }
 };
