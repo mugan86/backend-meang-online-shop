581d2f7d28efb2c0beb67e226872336dac4c6a8d
Anartz Mugika Ledo <mugan86@gmail.com>
2020-10-24 15:32:07 +0200

    Solucionado minibug que no asignadaba correctamente el ID a los elementos como tag, género

--- src/services/genres.service.ts
+++ src/services/genres.service.ts
@@ -45,7 +45,7 @@ class GenresService extends ResolversOperationsService {
         }
         // Si valida las opciones anteriores, venir aquí y crear el documento
         const genreObject = {
-            id: await asignDocumentId(this.getDb(), this.collection, { id: -1}),
+            id: await asignDocumentId(this.getDb(), this.collection, { _id: -1}),
             name: genre,
             slug: slugify(genre || '', { lower: true })
         };
