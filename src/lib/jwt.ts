d526f00d832bc74b56fc7812cb3a975c75dad9f5
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-20 12:32:23 +0100

    Log en desarrollo con el token

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -3,25 +3,24 @@ import { SECRET_KEY, MESSAGES, EXPIRETIME } from './../config/constants';
 import jwt from 'jsonwebtoken';
 
 class JWT {
-    private secretKey = SECRET_KEY as string;
-    // Informaci´pn del payload con fecha de caducidad 24 horas por defecto
-    sign(data: IJwt, expiresIn: number = EXPIRETIME.H24 ) {
-        return jwt.sign(
-            { user: data.user },
-            this.secretKey,
-            { expiresIn} 
-        );
+  private secretKey = SECRET_KEY as string;
+  // Informaci´pn del payload con fecha de caducidad 24 horas por defecto
+  sign(data: IJwt, expiresIn: number = EXPIRETIME.H24) {
+    return jwt.sign({ user: data.user }, this.secretKey, { expiresIn });
+  }
+
+  verify(token: string) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('token:::', token);
+      console.log('Verifiy-key', this.secretKey);
     }
 
-    verify(token: string) {
-        console.log('token:::', token);
-        console.log('Verifiy-key', this.secretKey);
-        try {
-            return jwt.verify(token, this.secretKey);
-        } catch (e) {
-            return MESSAGES.TOKEN_VERICATION_FAILED;
-        }
+    try {
+      return jwt.verify(token, this.secretKey);
+    } catch (e) {
+      return MESSAGES.TOKEN_VERICATION_FAILED;
     }
+  }
 }
 
 export default JWT;
