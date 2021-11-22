f5dfe6d7b154b6c2f7e48a5ead3a9cfb2b751a8c
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-22 15:09:54 +0100

    Resolvers - Query - users.graphql - login OK

--- test/2-resolvers/1-query/1-users.graphql/1-users.test.js
+++ test/2-resolvers/1-query/1-users.graphql/1-users.test.js
@@ -2,7 +2,8 @@ const EasyGraphQLTester = require('easygraphql-tester');
 const { it } = require('mocha');
 
 const apiSchema = require('../../../mocks/api-schema');
-const resolvers = require('./../../../mocks/resolvers/query/users/users').resolverUsersQueries;
+const resolvers =
+  require('./../../../mocks/resolvers/query/users/users').resolverUsersQueries;
 const expect = require('chai').expect;
 
 describe('Test Resolvers - Query - user', () => {
@@ -10,7 +11,7 @@ describe('Test Resolvers - Query - user', () => {
   before(function () {
     tester = new EasyGraphQLTester(apiSchema, resolvers);
   });
-  it("'users'", async () => {
+  it("'users' - Lista de usuarios", async () => {
     const query = `
     {
       users {
@@ -58,11 +59,80 @@ describe('Test Resolvers - Query - user', () => {
         expect(resultUsers[0][key]).to.be.a('string');
         if (key === 'role') {
           // Comprobar si Role es mayúsculas
-          expect(resultUsers[0][key]).to.equal(resultUsers[0][key].toUpperCase());
-          expect(resultUsers[0][key]).to.not.equal(resultUsers[0][key].toLowerCase());
+          expect(resultUsers[0][key]).to.equal(
+            resultUsers[0][key].toUpperCase()
+          );
+          expect(resultUsers[0][key]).to.not.equal(
+            resultUsers[0][key].toLowerCase()
+          );
         }
       }
     });
   });
-  
+  it("'login' - Iniciar sesión - Credenciales correctos", async () => {
+    const query = `
+    query startSession($email: String!, $password: String!){
+      login(email: $email, password: $password) {
+        status
+        message
+        token
+      }
+    }
+          `;
+    const result = await tester.graphql(query, undefined, undefined, {
+      email: 'mugan86@gmail.com',
+      password: '123456',
+    });
+    const resultData = result.data.login;
+    expect(resultData.status).to.be.a('boolean');
+    expect(resultData.message).to.be.a('string');
+    expect(resultData.token).to.be.a('string');
+    expect(resultData.status).to.equal(true);
+    expect(resultData.message).to.equal('Usuario cargado correctamente');
+    expect(resultData.token.includes('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9')).to.equal(true);
+  });
+
+  it("'login' - Iniciar sesión - Credenciales incorrectos", async () => {
+    const query = `
+    query startSession($email: String!, $password: String!){
+      login(email: $email, password: $password) {
+        status
+        message
+        token
+      }
+    }
+          `;
+    const result = await tester.graphql(query, undefined, undefined, {
+      email: 'mugan86@gmail.com',
+      password: '1234',
+    });
+    const resultData = result.data.login;
+    expect(resultData.status).to.be.a('boolean');
+    expect(resultData.message).to.be.a('string');
+    expect(resultData.status).to.equal(false);
+    expect(resultData.message).to.equal('Password y usuario no son correctos, sesión no iniciada');
+    expect(resultData.token).to.equal(null);
+  });
+
+  it("'login' - Iniciar sesión - Usuario no existe", async () => {
+    const query = `
+    query startSession($email: String!, $password: String!){
+      login(email: $email, password: $password) {
+        status
+        message
+        token
+      }
+    }
+          `;
+    const result = await tester.graphql(query, undefined, undefined, {
+      email: '',
+      password: '',
+    });
+    const resultData = result.data.login;
+    expect(resultData.status).to.be.a('boolean');
+    expect(resultData.message).to.be.a('string');
+    expect(resultData.status).to.equal(false);
+    expect(resultData.message).to.equal('Usuario no existe');
+    expect(resultData.token).to.equal(null);
+  });
 });
--- test/mocks/resolvers/query/users/users.js
+++ test/mocks/resolvers/query/users/users.js
@@ -26,17 +26,36 @@ const users = () => {
   };
 };
 
-const helloWithName = (_, args) => {
-  return `Hello ${args.name}!!`;
-};
-
-const helloToGraphQLCourse = () => {
-  return 'Hello to GraphQL Course!!';
+const login = (_, args) => {
+  const email = args.email;
+  const password = args.password;
+  if (email === '' || email === null) {
+    return {
+      status: false,
+      message: 'Usuario no existe',
+      token: null,
+    };
+  }
+  if (email === 'mugan86@gmail.com' && password === '123456') {
+    return {
+      status: true,
+      message: 'Usuario cargado correctamente',
+      token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjVlOGVmYzA3MmQ1MTI1MDlmZmY3ZTg1YyIsIm5hbWUiOiJBbmFydHoiLCJsYXN0bmFtZSI6Ik11Z2lrYSBMZWRvIiwiZW1haWwiOiJtdWdhbjg2QGdtYWlsLmNvbSIsInJvbGUiOiJBRE1JTiIsImlkIjoiMSIsImFjdGl2ZSI6ZmFsc2UsInN0cmlwZUN1c3RvbWVyIjoiY3VzX0hhZmZKUXJsT1pxYnFBIn0sImlhdCI6MTYzNzU4ODI1NSwiZXhwIjoxNjM3Njc0NjU1fQ.haueIFJEN45njp8dl6diEKT3Ls1shqbCs-bp6tVXeTI',
+    };
+  }
+  return {
+    status: false,
+    message: 'Password y usuario no son correctos, sesión no iniciada',
+    token: null
+  };
+    
 };
 
 const resolverUsersQueries = {
   Query: {
     users,
+    login
   },
 };
 
